b2a88837eedfa4eb83b61875470724e2826e2eba
mozby69 <140866402+mozby69@users.noreply.github.com>
2024-06-28 21:31:40 +0800

    Add files via upload

---
+++ question.js
@@ -0,0 +1,573 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Natural barriers of the immune system include all except which of the following?",
+    answer: "Hair follicles",
+    options: [
+      "pH of secretions",
+      "Coughing",
+      "Hair follicles",
+      "Intestinal bacteria"
+    ]
+  },
+  {
+    numb: 2,
+    question: "The fundamental difference between primary and secondary organs of the lymphatic system is:",
+    answer: "Maturation of lymphocytes occurs in primary organs, and activation occurs in secondary organs",
+    options: [
+      "Antibody production occurs only in the primary lymph organs",
+      "Complement production occurs only in the primary lymph organs",
+      "Maturation of lymphocytes occurs in secondary organs, and activation occurs in primary organs",
+      "Maturation of lymphocytes occurs in primary organs, and activation occurs in secondary organs"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Toll-like receptors act in which way?",
+    answer: "Enhance recognition of bacteria by phagocytic cells",
+    options: [
+      "Enhance recognition of bacteria by phagocytic cells",
+      "Activate B cells to produce antibody",
+      "Activate helper T cells",
+      "Aid in processing antigen in the form of an MHC molecule"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Neutrophils and monocytes have receptors for which part of the immunoglobulin molecule?",
+    answer: "Fc",
+    options: [
+      "Fc",
+      "Fab",
+      "Hinge region",
+      "Variable region"
+    ]
+  },
+  {
+    numb: 5,
+    question: "One B-cell marker of early-stage B-cell development is _____, whereas ____ is a marker for later stages of B-cell development.",
+    answer: "CD10; CD20",
+    options: [
+      "CD20; CD10",
+      "CD21; CD10",
+      "CD10; CD20",
+      "CD19; CD10"
+    ]
+  },
+  {
+    numb: 6,
+    question: "A double-positive T cell would express which markers?",
+    answer: "CD4+CD8+CD3+",
+    options: [
+      "CD4+CD8+CD3+",
+      "CD4CD8+CD3+",
+      "CD4CD8CD3",
+      "CD4+CD8-CD3+"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which cell is considered to be a bridge between the innate and adaptive immune systems?",
+    answer: "NK cell",
+    options: [
+      "NK cell",
+      "Mast cell",
+      "Monocyte-macrophage",
+      "T cell"
+    ]
+  },
+  {
+    numb: 8,
+    question: "_______ are involved in cell-mediated immunity, whereas _______ are involved in humoral immunity.",
+    answer: "A and B",
+    options: [
+      "T cells; B cells",
+      "T cells; antibodies",
+      "B cells; T cells",
+      "A and B"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Antigens that make very good immunogens include which of the following?",
+    answer: "Proteins",
+    options: [
+      "Carbohydrates",
+      "Proteins",
+      "Both a and b",
+      "Neither a or b"
+    ]
+  },
+  {
+    numb: 10,
+    question: "The function of the complement system include(s) which of the following?",
+    answer: "All of the above",
+    options: [
+      "Clearance of cellular debris",
+      "Chemotaxis",
+      "Lysis of bacteria",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 11,
+    question: "When C3 is cleaved by C3 convertase, what is the result?",
+    answer: "All of the above",
+    options: [
+      "C3a is released",
+      "C3b is used as an opsonin",
+      "C3b is combined with other complement proteins to form C5 convertase",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Characteristics of cytokines include which of the following?",
+    answer: "All of the above",
+    options: [
+      "They can have a pleomorphic effect",
+      "Cytokines are redundant",
+      "Cytokines enhance cellular differentiation of lymphocytes",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Immunoglobulin idiotypes are antibodies with variations in the domains of which of the following?",
+    answer: "VH and VL",
+    options: [
+      "CH1 and CH2",
+      "VH and VL",
+      "VH and CL",
+      "CH1, CH2, and CH3"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Mannose-binding lectin is similar to which component of the classical pathway?",
+    answer: "C1q",
+    options: [
+      "C3",
+      "C2",
+      "C1q",
+      "C5a"
+    ]
+  },
+  {
+    numb: 15,
+    question: "A patient with a viral infection to the ABC virus is found to have a high antibody titer to the ABC virus’s RNA, or anti-ABCr. Which of the following is true?",
+    answer: "MHC class I molecules presented antigen to CD8+ T cells",
+    options: [
+      "MHC class I molecules presented antigen to CD4+ T cells",
+      "MHC class II molecules presented antigen to CD8+ T cells",
+      "MHC class I molecules presented antigen to CD8+ T cells",
+      "MHC class II molecules presented antigen to CD4+ T cells"
+    ]
+  },
+
+
+  //
+
+
+
+    // Previous questions from 1 to 15
+    {
+      numb: 16,
+      question: "What is the main difference between agglutination and precipitation reactions?",
+      answer: "Agglutination occurs when the antigen is particulate",
+      options: [
+        "Agglutination occurs between a soluble antigen and antibody",
+        "Agglutination occurs when the antigen is particulate",
+        "Precipitation occurs when the antigen is particulate",
+        "Precipitation occurs when both antigen and antibody are particulate"
+      ]
+    },
+    {
+      numb: 17,
+      question: "Postzone causes false-negative reactions in antibody titers as a result of which of the following?",
+      answer: "Excess antigen in test",
+      options: [
+        "Too much diluent added to test",
+        "Excess antibody in test",
+        "Excess antigen in test",
+        "Incorrect diluent added to test"
+      ]
+    },
+    {
+      numb: 18,
+      question: "Antibodies produced against two or more epitopes of specific antigen are considered ____________.",
+      answer: "Polyclonal",
+      options: [
+        "Monoclonal",
+        "Pleomorphic",
+        "Dimorphic",
+        "Polyclonal"
+      ]
+    },
+    {
+      numb: 19,
+      question: "In the radial immunodiffusion test, the gel contains which of the following?",
+      answer: "Antibody",
+      options: [
+        "The antigen to be tested",
+        "Antibody",
+        "Patient sample",
+        "None of the above; the gel is the medium to which the antibody and antigen are applied in equal proportion"
+      ]
+    },
+    {
+      numb: 20,
+      question: "Which statement is true regarding the radial immunodiffusion test?",
+      answer: "The area of the precipitin ring is directly proportional to the concentration of antigen in the sample",
+      options: [
+        "The area of the precipitin ring is directly proportional to the concentration of antigen in the sample",
+        "The area of the precipitin ring is directly proportional to the concentration of antibody in the sample",
+        "The area of the precipitin ring is directly proportional to the concentration antibody and the antigen in the sample",
+        "The area of the precipitin ring indicates a partial identity to the antibody in the sample"
+      ]
+    },
+    {
+      numb: 21,
+      question: "The indirect antiglobulin test is for ______________, whereas the direct antiglobulin test is for __________.",
+      answer: "Serum antibody; bound antibody",
+      options: [
+        "Serum antigen; bound antigen",
+        "Serum antigen; bound antibody",
+        "Serum antibody; bound antigen",
+        "Serum antibody; bound antibody"
+      ]
+    },
+    {
+      numb: 22,
+      question: "In an indirect immunofluorescent antibody test for CMV antibodies, the conjugated antibody used for visualizing is:",
+      answer: "Antihuman globulin conjugated to a fluorescent dye",
+      options: [
+        "Antihuman globulin conjugated to a fluorescent dye",
+        "Anti-CMV antibody conjugated to a fluorescent dye",
+        "CMV virus conjugated to a fluorescent dye",
+        "Antihuman globulin conjugated to an enzyme"
+      ]
+    },
+    {
+      numb: 23,
+      question: "What is the difference between nephlometry and turbidimetry?",
+      answer: "Nephlometry measures light scattered in a solution, and turbidimetry measures light transmitted through a solution",
+      options: [
+        "There is no difference between the two assays, only in name",
+        "Nephlometry is a newer example of turbidimetry",
+        "Nephlometry measures light transmitted through a solution, and turbidimetry measures light scattered in a solution",
+        "Nephlometry measures light scattered in a solution, and turbidimetry measures light transmitted through a solution"
+      ]
+    },
+    {
+      numb: 24,
+      question: "In an Ouchterlony immunodiffusion, the line of precipitation between the antibody and the antigen wells forms an X. This reaction would be described as which of the following?",
+      answer: "Nonidentity",
+      options: [
+        "Nonidentity",
+        "Partial identity",
+        "Identity"
+      ]
+    },
+    {
+      numb: 25,
+      question: "An initial titer of 4 followed by a subsequent titer of 16 for the same patient, drawn 2 weeks later, is indicative of which of the following?",
+      answer: "Infection",
+      options: [
+        "Infection",
+        "Convalescence",
+        "Past exposure",
+        "No exposure"
+      ]
+    },
+    {
+      numb: 26,
+      question: "A deficiency of T cells can result in which of the following?",
+      answer: "Fewer B cells maturing to plasma cells",
+      options: [
+        "Low levels of complement",
+        "Dysfunctional macrophages",
+        "Fewer B cells maturing to plasma cells",
+        "Contact dermatitis"
+      ]
+    },
+    {
+      numb: 27,
+      question: "A 2-week-old baby is seen for a possible infection with CMV. Which of the following statements is false?",
+      answer: "An initial titer of anti-CMV IgG would need to be established",
+      options: [
+        "A positive anti-CMV result from baby’s specimen is inconclusive",
+        "An initial titer of anti-CMV IgG would need to be established",
+        "A positive result for anti-CMV IgM would indicate infection",
+        "All are false statements"
+      ]
+    },
+    {
+      numb: 28,
+      question: "What is the basic difference between the RPR and VDRL tests?",
+      answer: "The RPR test is read macroscopically, whereas the VDRL is read microscopically",
+      options: [
+        "The RPR detects antigen, whereas the VDRL detects antibody",
+        "The RPR test is read macroscopically, whereas the VDRL is read microscopically",
+        "The RPR test is a treponemal test, whereas the VDRL is nontreponemal",
+        "There is no difference because they are both specific test for syphilis"
+      ]
+    },
+
+    //
+
+  
+      // Previous questions from 1 to 28
+      {
+        numb: 29,
+        question: "A patient has the following hepatitis B serology: HBsAg: Negative, Anti-HBc: Positive, Anti-HBs: Positive. These results are consistent with which of the following?",
+        answer: "Recovery from hepatitis B",
+        options: [
+          "Acute hepatitis B",
+          "Chronic hepatitis B",
+          "Recovery from hepatitis B",
+          "Acute hepatitis A"
+        ]
+      },
+      {
+        numb: 30,
+        question: "The HLA genes are inherited as:",
+        answer: "Haplotypes: One haplotype from each parent",
+        options: [
+          "Diplotypes: Two diplotypes from each parent",
+          "Haplotypes: One haplotype from each parent",
+          "HLAs are not inherited, instead are proteins absorbed onto cells",
+          "Only the HLA-A antigen is an inheritable trait"
+        ]
+      },
+      {
+        numb: 31,
+        question: "Agglutination and precipitation that is visible depends on antigen–antibody ratios ________.",
+        answer: "That are equivalent",
+        options: [
+          "With antigen in excess",
+          "With antibody in excess",
+          "That are equivalent",
+          "All of the above"
+        ]
+      },
+      {
+        numb: 32,
+        question: "Which of the following cell types is implicated in immediate hypersensitivity?",
+        answer: "Mast cell",
+        options: [
+          "Neutrophil",
+          "Mast cell",
+          "Macrophage",
+          "Monocyte"
+        ]
+      },
+      {
+        numb: 33,
+        question: "Anti-dsDNA antibodies are associated with which of the following?",
+        answer: "Systemic lupus erythematosus",
+        options: [
+          "Syphilis",
+          "CMV infection",
+          "Systemic lupus erythematosus",
+          "Hemolytic anemia"
+        ]
+      },
+      {
+        numb: 34,
+        question: "Rheumatoid factor is typically an IgM autoantibody with specificity for which of the following?",
+        answer: "Fc portion of IgG",
+        options: [
+          "SS-B",
+          "Double-stranded DNA",
+          "Ribonucleoprotein",
+          "Fc portion of IgG"
+        ]
+      },
+      {
+        numb: 35,
+        question: "All of the following are autoimmune diseases except:",
+        answer: "Rh disease of the fetus and newborn",
+        options: [
+          "Rheumatoid arthritis",
+          "Rh disease of the fetus and newborn",
+          "Grave’s disease",
+          "Myasthenia gravis"
+        ]
+      },
+      {
+        numb: 36,
+        question: "In Grave’s disease, one of the main autoantibodies is:",
+        answer: "Antibody to TSHr",
+        options: [
+          "Anti-CCP",
+          "Antibody to islet cells of pancreas",
+          "Antibody to TSHr",
+          "Anti dsDNA"
+        ]
+      },
+      {
+        numb: 37,
+        question: "An autoantibody found in patients with which of the following?",
+        answer: "Thyroglobulin",
+        options: [
+          "TSH receptor",
+          "Islet cells",
+          "CRP",
+          "Thyroglobulin"
+        ]
+      },
+      {
+        numb: 38,
+        question: "Skin testing for exposure to tuberculosis is an example of which type of hypersensitivity?",
+        answer: "Type IV",
+        options: [
+          "Type I",
+          "Type II",
+          "Type III",
+          "Type IV"
+        ]
+      },
+      {
+        numb: 39,
+        question: "Which of the following is a test for specific treponemal antibody?",
+        answer: "FTA-ABS",
+        options: [
+          "VDRL",
+          "RPR",
+          "FTA-ABS",
+          "All of the above"
+        ]
+      },
+      {
+        numb: 40,
+        question: "Serum tested positive for HBsAg and anti-HBc IgM. The patient most likely has which of the following?",
+        answer: "Acute hepatitis B",
+        options: [
+          "Acute hepatitis C",
+          "Chronic hepatitis B",
+          "Acute hepatitis B",
+          "Acute hepatitis A"
+        ]
+      },
+      {
+        numb: 41,
+        question: "The main difference between leukemias and lymphomas is which of the following?",
+        answer: "Leukemias are malignancies of cells in the bone marrow",
+        options: [
+          "Leukemias are malignancies of cells in the bone marrow",
+          "Lymphomas are malignancies of cells in the bone marrow",
+          "Lymphomas are classified as either acute or chronic",
+          "Leukemias are malignancies in lymphoid tissue"
+        ]
+      },
+      {
+        numb: 42,
+        question: "A 1-year-old boy is seen for having many recurrent infections with Streptococcus pneumoniae. Laboratory tests revealed a normal quantity of T cells, but no B cells and no immunoglobulins were seen on electrophoresis. Which of the following would most likely be the cause?",
+        answer: "Bruton’s agammaglobulinemia",
+        options: [
+          "Chronic granulomatous disease",
+          "Bruton’s agammaglobulinemia",
+          "DiGeorge’s syndrome",
+          "Wiskott-Aldrich syndrome"
+        ]
+      },
+      {
+        numb: 43,
+        question: "A patient with hereditary angioedema has which of the following deficiencies?",
+        answer: "C1 Inhibitor",
+        options: [
+          "C5-9",
+          "Phagocytic cell function",
+          "Mature B cells",
+          "C1 Inhibitor"
+        ]
+      },
+
+      //
+
+ 
+        // Previous questions from 1 to 43
+        {
+          numb: 44,
+          question: "A radiograph of a 1-year-old boy indicates the lack of a thymus. Complete blood count and flow cytometry confirm a below-normal lymphocyte count and a lack of T cells. Which of the following would most likely be the cause?",
+          answer: "DiGeorge’s syndrome",
+          options: [
+            "DiGeorge’s syndrome",
+            "Wiskott-Aldrich syndrome",
+            "Bare lymphocyte syndrome",
+            "Bruton’s agammaglobulinemia"
+          ]
+        },
+        {
+          numb: 45,
+          question: "Severe combined immunodeficiency is characterized by which of the following?",
+          answer: "All of the above",
+          options: [
+            "Diagnosed in infancy",
+            "Shortened life span",
+            "No antibody production",
+            "All of the above"
+          ]
+        },
+        {
+          numb: 46,
+          question: "A 3-year-old boy is seen by his physician because of many recent bacterial infections. Flow cytometry indicates normal levels of T and B cells. The nitroblue tetrazolium test for oxidative reduction is negative. The most likely cause is:",
+          answer: "Chronic granulomatous disease",
+          options: [
+            "Wegener’s syndrome",
+            "Chronic granulomatous disease",
+            "Bruton’s agammaglobulinemia",
+            "Diabetes mellitus"
+          ]
+        },
+        {
+          numb: 47,
+          question: "A 25-year-old man was seen by his physician for recurrent infections. Immunoelectrophoresis revealed hypogammaglobulinemia. This man most likely has which of the following?",
+          answer: "Common variable immunodeficiency",
+          options: [
+            "Bruton’s agammaglobulinemia",
+            "Common variable immunodeficiency",
+            "X-linked agammaglobulinemia",
+            "DiGeorge’s syndrome"
+          ]
+        },
+        {
+          numb: 48,
+          question: "In which disease would you expect to see an IgM spike on electrophoresis?",
+          answer: "Waldenström’s macroglobulinemia",
+          options: [
+            "Transient hypogammaglobulinemia of infancy",
+            "Wiskott-Aldrich syndrome",
+            "Leukocyte adhesion disease",
+            "Waldenström’s macroglobulinemia"
+          ]
+        },
+        {
+          numb: 49,
+          question: "A person has an infected bug bite with pain, swelling, and redness. What is the cause of these physical symptoms of inflammation?",
+          answer: "Increased blood flow and neutrophils to site",
+          options: [
+            "Production of antibody",
+            "Secondary immune response",
+            "Increased blood flow and neutrophils to site",
+            "Activation of NK cells"
+          ]
+        },
+        {
+          numb: 50,
+          question: "The type of graft rejection that occurs within minutes of a tissue transplant is ____________.",
+          answer: "Hyperacute",
+          options: [
+            "Acute",
+            "Chronic",
+            "Hyperacute",
+            "Accelerated"
+          ]
+        }
+ 
+      
+    
+    
+
+];
